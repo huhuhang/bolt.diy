af663e70684198b02809b2270def7695d0d9d7c3
huhuhang <5147530+huhuhang@users.noreply.github.com>
2025-03-18 21:44:34 +0800

    chore: update font styles in root component and set default font family in global styles

--- app/root.tsx
+++ app/root.tsx
@@ -27,17 +27,8 @@ export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: globalStyles },
   { rel: 'stylesheet', href: xtermStyles },
   {
-    rel: 'preconnect',
-    href: 'https://fonts.googleapis.com',
-  },
-  {
-    rel: 'preconnect',
-    href: 'https://fonts.gstatic.com',
-    crossOrigin: 'anonymous',
-  },
-  {
     rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
+    href: 'https://chinese-fonts-cdn.deno.dev/packages/maple-mono-cn/dist/MapleMono-CN-Regular/result.css',
   },
 ];
 
